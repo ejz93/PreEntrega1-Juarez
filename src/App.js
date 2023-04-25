5c00d25ffeacf72d43f9feef1b9548b6e7c6b79d
ejz93 <juarezestanislao93@gmail.com>
2023-04-25 19:05:44 -0400

    Arreglos varios + MUI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
-import NavBar from './components/Navbar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import Container from './layouts/Container'
+// import Home from './pages/Home'
+// import About from './pages/About'
+// import Products from './pages/Products'
+// import Contact from './pages/Contact'
 
 function App() {
+
   return (
       <div className="App">
-      <NavBar />
-      <ItemListContainer greeting={'Greetings!'} />
+        <Container>
+        <ItemListContainer greeting={'Greetings!'} />
+          {/* <Home/>
+          <About/>
+          <Products/>
+          <Contact/> */}
+        </Container>
+      {/* <NavBar className={"navbar navbar-expand-lg navbar-light bg-warning"} navbarLogo={'Elementary'} />
+      <ItemListContainer greeting={'Greetings!'} /> */}
       </div>
   );
 }
---
+++ src/components/CartWidget/CartWidgetView.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Badge, IconButton} from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+const CartWidgetView = ({color}) => {
+  console.log('Contenido de color: =>', color);
+  return (
+    <IconButton
+      size='large'
+      aria-label='show shopping'
+      color={color}
+      sx={{pt: 1, pb: 0}}
+    >
+      <Badge badgeContent={1} color='info'>
+        <ShoppingCartIcon sx={{pb: 0}} />
+      </Badge>
+    </IconButton>
+  );
+};
+
+export default CartWidgetView;
